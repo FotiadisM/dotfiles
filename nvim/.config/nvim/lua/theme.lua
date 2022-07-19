be9dc6f3d2741379dd6aa010e1a7eb07e012c67a
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2022-07-19 18:19:01 +0300

    nvim: lualin custom theme

--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -2,9 +2,55 @@ vim.g.tokyonight_style = "night"
 vim.g.tokyonight_transparent_sidebar = true
 vim.cmd([[colorscheme tokyonight]])
 
+-- custom tokyonight inspired theme
+local colors = {
+	dark = "#1f2335",
+	white = "#ffffff",
+	gray = "#CCCCCC",
+	blue = "#2ac3de",
+	purple = "#9d7cd8",
+	orange = "#ff9e64",
+	green = "#9ece6a",
+	teal = "#1abc9c",
+	red = "#f7768e",
+}
+
+local theme = {
+	normal = {
+		a = { bg = colors.blue, fg = colors.dark, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.white },
+		c = { bg = colors.dark, fg = colors.gray },
+	},
+	insert = {
+		a = { bg = colors.teal, fg = colors.dark, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.white },
+		c = { bg = colors.dark, fg = colors.gray },
+	},
+	visual = {
+		a = { bg = colors.green, fg = colors.dark, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.white },
+		c = { bg = colors.dark, fg = colors.gray },
+	},
+	replace = {
+		a = { bg = colors.red, fg = colors.dark, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.white },
+		c = { bg = colors.dark, fg = colors.gray },
+	},
+	command = {
+		a = { bg = colors.orange, fg = colors.dark, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.white },
+		c = { bg = colors.dark, fg = colors.gray },
+	},
+	inactive = {
+		a = { bg = colors.purple, fg = colors.gray, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.white },
+		c = { bg = colors.dark, fg = colors.gray },
+	},
+}
+
 require("lualine").setup({
 	options = {
-		theme = "tokyonight",
+		theme = theme,
 	},
 	extensions = { "nvim-tree" },
 })
