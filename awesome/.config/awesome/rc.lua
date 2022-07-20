55903160beb4a40dadc6e91b23e5f964dcde407c
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2022-07-20 21:12:40 +0300

    awesome: changed layout to tiling

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -72,8 +72,7 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.floating,
+	awful.layout.suit.tile,
 }
 -- }}}
 
