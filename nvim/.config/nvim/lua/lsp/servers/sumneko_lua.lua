c28d272fd1467aa0455fc336c2ab3d904b28f611
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2022-11-25 01:47:05 +0200

    nvim: sumneko_lua changes

--- nvim/.config/nvim/lua/lsp/servers/sumneko_lua.lua
+++ nvim/.config/nvim/lua/lsp/servers/sumneko_lua.lua
@@ -1,21 +1,16 @@
 local lua = {}
 
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 function lua.setup(config, on_attach)
 	config.settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
+				workspaceDelay = 1000,
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
