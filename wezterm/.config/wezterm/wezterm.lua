8909d65e461201b1daafd8663d32cdebc29c4982
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-03-21 20:23:01 +0200

    wezterm: remove comments

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,30 +1,5 @@
 local wezterm = require("wezterm")
 
--- colors:
---   primary:
---     background: "#0E1018"
---     foreground: "#ebdbb2"
---
---   normal:
---     black: "#282828"
---     red: "#cc241d"
---     green: "#98971a"
---     yellow: "#d79921"
---     blue: "#458588"
---     magenta: "#b16286"
---     cyan: "#689d6a"
---     white: "#dfaf87"
---
---   bright:
---     black: "#928374"
---     red: "#fb4934"
---     green: "#b8bb26"
---     yellow: "#fabd2f"
---     blue: "#83a598"
---     magenta: "#d3869b"
---     cyan: "#8ec07c"
---     white: "#ebdbb2"
-
 local gruvbox = {
 	foreground = "#ebdbb2",
 	background = "#0E1018",
