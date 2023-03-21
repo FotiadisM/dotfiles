095997c726722780a1e1ac03fe61a14cab94afe7
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-03-21 22:19:48 +0200

    nvim: changed winbar

--- nvim/.config/nvim/lua/util/winbar.lua
+++ nvim/.config/nvim/lua/util/winbar.lua
@@ -4,7 +4,9 @@ vim.api.nvim_set_hl(0, "WinBarModified", { fg = "#fd7f20" })
 
 function M.set_winbar()
 	local modified = vim.api.nvim_eval_statusline("%M", {}).str == "+" and "●" or ""
-	return "%=%#WinBarModified#" .. modified .. "%* %f"
+	modified = "%#WinBarModified#" .. modified .. "%* "
+	local filename = vim.fn.expand("%:~:.")
+	return "  " .. modified .. filename
 end
 
 return M
