b296371e29653ad6ecec9cf069965a2c6fbee0a5
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-03-21 22:20:24 +0200

    nvim: added new theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,6 +34,8 @@ require("lazy").setup("plugins", {
 	},
 })
 
+vim.cmd.colorscheme("gruvbox")
+
 -- if nvim starts with a directory as an argument, cd into it
 if vim.fn.argc() == 1 then
 	local arg = vim.fn.argv()[1]
