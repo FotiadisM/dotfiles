aab8fd32dbf349112e9ab956ebe38ab9b574a2e3
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-03-29 23:20:18 +0300

    nvim: added nvim-navbuddy

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,92 +1,101 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"j-hui/fidget.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"j-hui/fidget.nvim",
 
-		"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp",
 
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"jayp0521/mason-null-ls.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"jayp0521/mason-null-ls.nvim",
 
-		{ "jose-elias-alvarez/null-ls.nvim", dependencies = "nvim-lua/plenary.nvim" },
+			{ "jose-elias-alvarez/null-ls.nvim", dependencies = "nvim-lua/plenary.nvim" },
 
-		"folke/neodev.nvim",
-		"simrat39/rust-tools.nvim",
-		"jose-elias-alvarez/typescript.nvim",
-		"b0o/schemastore.nvim",
-		{ "someone-stole-my-name/yaml-companion.nvim", dependencies = "nvim-lua/plenary.nvim" },
-	},
-	lazy = false,
-	config = function()
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-		vim.lsp.handlers["textDocument/signatureHelp"] =
-			vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+			"folke/neodev.nvim",
+			"simrat39/rust-tools.nvim",
+			"jose-elias-alvarez/typescript.nvim",
+			"b0o/schemastore.nvim",
+			{ "someone-stole-my-name/yaml-companion.nvim", dependencies = "nvim-lua/plenary.nvim" },
 
-		vim.diagnostic.config({
-			virtual_text = {
-				prefix = "●",
-				source = "if_many",
-			},
-			float = {
-				source = "if_many",
-			},
-		})
+			"SmiteshP/nvim-navbuddy",
+		},
+		lazy = false,
+		config = function()
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
-		local on_attach = require("lsp").on_attach
-		local make_config = require("lsp").make_config
-
-		require("fidget").setup({
-			text = { spinner = "dots" },
-			sources = {
-				["null-ls"] = {
-					ignore = true,
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+					source = "if_many",
 				},
-			},
-		})
+				float = {
+					source = "if_many",
+				},
+			})
 
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			automatic_installation = { exclude = { "clangd", "rust_analyzer" } },
-		})
+			local on_attach = require("lsp").on_attach
+			local make_config = require("lsp").make_config
 
-		local lspconf = require("lspconfig")
+			require("fidget").setup({
+				text = { spinner = "dots" },
+				sources = {
+					["null-ls"] = {
+						ignore = true,
+					},
+				},
+			})
 
-		require("neodev").setup()
-		lspconf["lua_ls"].setup(require("lsp.servers.sumneko_lua").setup(make_config(), on_attach))
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				automatic_installation = { exclude = { "clangd", "rust_analyzer" } },
+			})
 
-		lspconf["gopls"].setup(require("lsp.servers.gopls").setup(make_config(), on_attach))
-		lspconf["texlab"].setup(require("lsp.servers.texlab").setup(make_config(), on_attach))
-		lspconf["html"].setup(require("lsp.servers.html").setup(make_config(), on_attach))
-		lspconf["jsonls"].setup(require("lsp.servers.jsonls").setup(make_config(), on_attach))
-		lspconf["yamlls"].setup(require("lsp.servers.yamlls").setup(make_config(), on_attach))
-		lspconf["volar"].setup(require("lsp.servers.volar").setup(make_config(), on_attach))
-		require("typescript").setup({
-			server = require("lsp.servers.tsserver").setup(make_config(), on_attach),
-		})
-		require("rust-tools").setup({
-			server = require("lsp.servers.rust_analyzer").setup(make_config(), on_attach),
-		})
-		local clangd = make_config()
-		clangd.filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }
-		lspconf["clangd"].setup(clangd)
-		lspconf["bashls"].setup(make_config())
-		lspconf["dockerls"].setup(make_config())
-		lspconf["tailwindcss"].setup(make_config())
-		lspconf["terraformls"].setup(make_config())
+			local lspconf = require("lspconfig")
 
-		require("lsp.servers.null_ls").setup(on_attach)
-		require("mason-null-ls").setup({ automatic_installation = true })
+			require("neodev").setup()
+			lspconf["lua_ls"].setup(require("lsp.servers.sumneko_lua").setup(make_config(), on_attach))
 
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-		end
+			lspconf["gopls"].setup(require("lsp.servers.gopls").setup(make_config(), on_attach))
+			lspconf["texlab"].setup(require("lsp.servers.texlab").setup(make_config(), on_attach))
+			lspconf["html"].setup(require("lsp.servers.html").setup(make_config(), on_attach))
+			lspconf["jsonls"].setup(require("lsp.servers.jsonls").setup(make_config(), on_attach))
+			lspconf["yamlls"].setup(require("lsp.servers.yamlls").setup(make_config(), on_attach))
+			lspconf["volar"].setup(require("lsp.servers.volar").setup(make_config(), on_attach))
+			require("typescript").setup({
+				server = require("lsp.servers.tsserver").setup(make_config(), on_attach),
+			})
+			require("rust-tools").setup({
+				server = require("lsp.servers.rust_analyzer").setup(make_config(), on_attach),
+			})
+			local clangd = make_config()
+			clangd.filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }
+			lspconf["clangd"].setup(clangd)
+			lspconf["bashls"].setup(make_config())
+			lspconf["dockerls"].setup(make_config())
+			lspconf["tailwindcss"].setup(make_config())
+			lspconf["terraformls"].setup(make_config())
 
-		return {
-			on_attach = on_attach,
-			make_config = make_config,
-		}
-	end,
+			require("lsp.servers.null_ls").setup(on_attach)
+			require("mason-null-ls").setup({ automatic_installation = true })
+
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+			end
+
+			return {
+				on_attach = on_attach,
+				make_config = make_config,
+			}
+		end,
+	},
+	{
+		"SmiteshP/nvim-navbuddy",
+		dependencies = { "neovim/nvim-lspconfig", "SmiteshP/nvim-navic", "MunifTanjim/nui.nvim" },
+		opts = { lsp = { auto_attach = true } },
+	},
 }
