31f1ccfd350bf43326f0dc9dd753c6b756ca1182
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-04-15 17:13:04 +0300

    nvim: lazy load treesitter plugins

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,79 +1,85 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
+	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+	{
 		"nvim-treesitter/nvim-treesitter-context",
-		"nvim-treesitter/playground",
+		event = "BufReadPre",
+		config = true,
 	},
-	lazy = false,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = "all",
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = false,
-			},
-			incremental_selection = {
-				enable = true,
-			},
-			textobjects = {
-				select = {
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = "BufReadPre",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "all",
+				highlight = {
 					enable = true,
-					lookahead = true,
-					keymaps = {
-						["ab"] = "@block.outer",
-						["ib"] = "@block.inner",
-						["ac"] = "@call.outer",
-						["ic"] = "@call.inner",
-						["ao"] = "@class.outer",
-						["io"] = "@class.inner",
-						["ad"] = "@conditional.outer",
-						["id"] = "@conditional.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["al"] = "@loop.outer",
-						["il"] = "@loop.inner",
+				},
+				indent = {
+					enable = false,
+				},
+				incremental_selection = {
+					enable = true,
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["ab"] = "@block.outer",
+							["ib"] = "@block.inner",
+							["ac"] = "@call.outer",
+							["ic"] = "@call.inner",
+							["ao"] = "@class.outer",
+							["io"] = "@class.inner",
+							["ad"] = "@conditional.outer",
+							["id"] = "@conditional.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["al"] = "@loop.outer",
+							["il"] = "@loop.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
 					},
 				},
-				move = {
+				playground = {
 					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-					},
-					goto_next_end = {
-						["]F"] = "@function.outer",
-					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-					},
 				},
-				swap = {
+				query_linter = {
 					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
 				},
-			},
-			playground = {
-				enable = true,
-			},
-			query_linter = {
-				enable = true,
-			},
-			autotag = { enable = true },
-			context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
-			},
-		})
-	end,
+				autotag = { enable = true },
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+			})
+		end,
+	},
 }
