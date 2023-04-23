cba2697b1155f1f8ed433fd775e904bc5cbeece0
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-04-23 16:52:46 +0300

    nvim: nvim-tree on_attach mappings

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -11,14 +11,6 @@ return {
 			view = {
 				adaptive_size = true,
 				preserve_window_proportions = true,
-				mappings = {
-					list = {
-						{ key = "l", action = "edit" },
-						{ key = "h", action = "close_node" },
-						{ key = "L", action = "vsplit" },
-						{ key = "s", action = "split" },
-					},
-				},
 			},
 			renderer = {
 				root_folder_modifier = ":t",
@@ -53,24 +45,23 @@ return {
 				show_on_dirs = true,
 				show_on_open_dirs = false,
 			},
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+
+				vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+				vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+				vim.keymap.set("n", "L", api.node.open.vertical, opts("Open: Vertical Split"))
+				vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
+			end,
 		})
 
 		require("nvim-tree.view").View.winopts.foldcolumn = "1"
-
-		-- quit nvim if the only window is NvimTree
-		vim.api.nvim_create_autocmd("BufEnter", {
-			group = vim.api.nvim_create_augroup("nvimtree_close", { clear = true }),
-			pattern = "NvimTree_*",
-			callback = function()
-				local layout = vim.api.nvim_call_function("winlayout", {})
-				if
-					layout[1] == "leaf"
-					and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
-					and layout[3] == nil
-				then
-					vim.cmd("confirm quit")
-				end
-			end,
-		})
 	end,
 }
