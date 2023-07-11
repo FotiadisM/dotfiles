f5abab06d67b440b9ac1d3cbc25eba9105b188b7
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2023-07-11 16:25:39 +0300

    nvim: plugin rename

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -13,7 +13,7 @@ return {
 		config = true,
 	},
 	{
-		"TimUntersberger/neogit",
+		"NeogitOrg/neogit",
 		cmd = "Neogit",
 		dependencies = "nvim-lua/plenary.nvim",
 		opts = { disable_commit_confirmation = true },
