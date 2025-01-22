ae232611a94794d404895b81cb9379c0c7c7b8f8
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2025-01-22 15:30:24 +0200

    nvim: added copilot

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,4 +1,6 @@
 local icons = {
+	Copilot = "",
+
 	Text = "",
 	Method = "",
 	Function = "",
@@ -27,45 +29,75 @@ local icons = {
 }
 
 return {
-	"saghen/blink.cmp",
-	version = "*",
-	dependencies = { "L3MON4D3/LuaSnip" },
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		keymap = {
-			preset = "default",
-			["<Tab>"] = {},
-			["<S-Tab>"] = {},
-			["<CR>"] = { "select_and_accept", "fallback" },
-			cmdline = {
-				preset = "default",
-			},
+	{
+		"zbirenbaum/copilot.lua",
+		opts = {
+			panel = { enabled = false },
+			suggestion = { enabled = false },
 		},
-		snippets = { preset = "luasnip" },
-
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
-
-		completion = {
-			menu = {
-				border = "rounded",
-				scrollbar = false,
-				draw = {
-					padding = 2,
-					columns = { { "kind_icon" }, { "label", "label_description", "kind", gap = 1 } },
-					components = {
-						kind_icon = {
-							text = function(ctx)
-								return icons[ctx.kind] .. ctx.icon_gap
-							end,
-						},
+	},
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		dependencies = { "L3MON4D3/LuaSnip", "giuxtaposition/blink-cmp-copilot" },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "copilot" },
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-cmp-copilot",
+						async = true,
+						-- score_offset = 25,
+						transform_items = function(_, items)
+							local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+							local kind_idx = #CompletionItemKind + 1
+							CompletionItemKind[kind_idx] = "Copilot"
+							for _, item in ipairs(items) do
+								item.kind = kind_idx
+							end
+							return items
+						end,
 					},
 				},
 			},
-			documentation = { auto_show = true, window = { border = "rounded" } },
+
+			keymap = {
+				preset = "default",
+				["<Tab>"] = {},
+				["<S-Tab>"] = {},
+				["<CR>"] = { "select_and_accept", "fallback" },
+				cmdline = {
+					preset = "default",
+				},
+			},
+
+			snippets = { preset = "luasnip" },
+
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+				kind_icons = icons,
+			},
+
+			completion = {
+				accept = {
+					auto_brackets = {
+						enabled = false,
+					},
+				},
+				menu = {
+					border = "rounded",
+					scrollbar = false,
+					draw = {
+						padding = 2,
+						columns = { { "kind_icon" }, { "label", "label_description", "kind", gap = 1 } },
+					},
+				},
+				documentation = { auto_show = true, window = { border = "rounded" } },
+			},
 		},
 	},
 }
