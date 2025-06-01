bbf335415f150dc548fd9e977c853ad8ed6066ca
Michalis Fotiadis <fotiadis.michalis20@gmail.com>
2025-06-01 18:00:48 +0300

    nvim: fix telescope-fzf-native build command

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+			build = "make",
 		},
 	},
 	config = function()
